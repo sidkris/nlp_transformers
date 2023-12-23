682a5b50a3df2fc191f9299321027fa71149e92f
Siddharth Krishnan <sidkrishnan@protonmail.com>
2023-12-23 22:04:07 +0530

    updates

---
+++ text_classification.py
@@ -0,0 +1,24 @@
+from transformers import pipeline
+from datasets import list_datasets
+import pandas as pd 
+import warnings
+warnings.filterwarnings("ignore")
+
+print("imports successful.")
+
+classifier = pipeline("text-classification")
+
+class TextClassifier:
+
+    @classmethod
+    def test(self):
+
+        sample_text = "what a great day!"
+
+        output = classifier(sample_text)
+
+        print(pd.DataFrame(output))
+
+
+if __name__ == "__main__":
+    TextClassifier.test()
